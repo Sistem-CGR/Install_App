17c9081ad61a5544e70ed719d94d14b7e40eb646
Sistem-CGR <sitemasrios@gmail.com>
2023-03-31 14:00:30 -0600

    Modificaciones

--- routes/modbus.js
+++ routes/modbus.js
@@ -1,198 +1,236 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
-app.use(cors({
-    origin: '*'
-}));
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Conexión a la BD
 //const { connection } = require('../config.db');
 
-
 // create a tcp modbus client
-const Modbus = require('jsmodbus')
-const net = require('net');
+const Modbus = require("jsmodbus");
+const net = require("net");
 
-const socket = new net.Socket()
-const clientPLC = new Modbus.client.TCP(socket)
+const socket = new net.Socket();
+const clientPLC = new Modbus.client.TCP(socket);
 const options = {
-    'host' : '192.168.1.50',
-    'port' : 502
-}
+  host: "192.168.1.50",
+  port: 502,
+};
 
 // Iniciamos la conexión con el plc
 socket.connect(options);
 
-socket.on('error', err =>  console.log(err));
+socket.on("error", (err) => console.log(err));
 
-
-const mqtt = require('mqtt')
+const mqtt = require("mqtt");
 //const mqttURL = 'ws://192.168.2.83:8083/mqtt'
-const mqttURL = 'ws://www.sistemaintegralrios.com/mqtt'
+const mqttURL = "ws://www.sistemaintegralrios.com/mqtt";
 
-const clientMQTT  = mqtt.connect(mqttURL)
+const clientMQTT = mqtt.connect(mqttURL);
 
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------\
 |                                                                               MQTT BROQUER                                                                            |
 \----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-clientMQTT.on('connect', function () {
-    // Subscribe to a topic
-    setInterval(() => {
-        let status = socket.resume()._readableState.destroyed
-        status ? socket.connect(options) : "";
+clientMQTT.on("connect", function () {
+  // Subscribe to a topic
+  setInterval(() => {
+    let status = socket.resume()._readableState.destroyed;
+    status ? socket.connect(options) : "";
 
-        clientMQTT.subscribe('/tst/#', function (err) {
-            if (!err) {
-                // Publish a message to a topic
-                clientPLC.readCoils(6, 8).then(result => {
-                    let Bobinas = result.response._body._valuesAsArray
-                    let arr = {
-                        "Inicio"    : Bobinas[0],
-                        "Arranque"  : Bobinas[1],
-                        "Paro"  : Bobinas[2],
-                        "Iniciado"  : Bobinas[0] == 0 ? false : true,
-                        "Paro de emergencia"  : Bobinas[4] ==  0 ? 'OFF' : 'ON',
-                        "Fallo de fase"  : Bobinas[5] ==  0 ? 'OFF' : 'ON',
-                    }
-                    clientMQTT.publish('/tst/Bobinas', JSON.stringify(arr))
-                    //clientMQTT.publish('/tst/Bobinas', JSON.stringify(Bobinas))
-                }).catch(function () {
-                    console.error( "clientMQTT readCoils " + require('util').inspect(arguments, { depth: null}))
-                    clientMQTT.publish('/tst/Bobinas', "OFFLINE")
-                })
-
-                // Publish a message to a topic
-                clientPLC.readHoldingRegisters(0, 16).then(result => {
-                    let Registros = result.response._body._values
-                    let arr = {
-                        "Hora"  : Registros[2] + ':' + Registros[1] + ':' + Registros[0],
-                        "Fehca" : Registros[5] + '/' + Registros[4] + '/' + Registros[3],
-                        "V12"   : Registros[6],
-                        "V23"   : Registros[7],
-                        "V31"   : Registros[8],
-                        "Flujo"   : Registros[9],
-                        "Presión"   : Registros[10],
-                        "Frecuencia"   : Registros[11],
-                        "Nivel dinamico"   : Registros[12],
-                        "I1"   : Registros[13],
-                        "I2"   : Registros[14],
-                        "I3"   : Registros[15]
-                    }
-                    clientMQTT.publish('/tst/Registros', JSON.stringify(arr))
-                    //clientMQTT.publish('/tst/Registros', JSON.stringify(Registros))
-                }).catch(function () {
-                    console.error( "clientMQTT readCoils " + require('util').inspect(arguments, { depth: null}))
-                    clientMQTT.publish('/tst/Registros', "OFFLINE")
-                })
-            }
+    clientMQTT.subscribe("tst/#", function (err) {
+      if (!err) {
+        // Publish a message to a topic
+        clientPLC
+          .readCoils(6, 8)
+          .then((result) => {
+            let Bobinas = result.response._body._valuesAsArray;
+            let arr = {
+              Inicio: Bobinas[0],
+              Arranque: Bobinas[1],
+              Paro: Bobinas[2],
+              Iniciado: Bobinas[0] == 0 ? false : true,
+              "Paro de emergencia": Bobinas[4] == 0 ? "OFF" : "ON",
+              "Fallo de fase": Bobinas[5] == 0 ? "OFF" : "ON",
+            };
+            clientMQTT.publish("tst/Bobinas", JSON.stringify(arr));
+            //clientMQTT.publish('/tst/Bobinas', JSON.stringify(Bobinas))
           })
-    }, 1000);
-})
+          .catch(function () {
+            console.error(
+              "clientMQTT readCoils " +
+                require("util").inspect(arguments, { depth: null })
+            );
+            clientMQTT.publish("tst/Bobinas", "OFFLINE");
+          });
+
+        // Publish a message to a topic
+        clientPLC
+          .readHoldingRegisters(0, 16)
+          .then((result) => {
+            let Registros = result.response._body._values;
+            let arr = {
+              Hora: Registros[2] + ":" + Registros[1] + ":" + Registros[0],
+              Fehca: Registros[5] + "/" + Registros[4] + "/" + Registros[3],
+              V12: Registros[6],
+              V23: Registros[7],
+              V31: Registros[8],
+              Flujo: Registros[9],
+              Presión: Registros[10],
+              Frecuencia: Registros[11],
+              "Nivel dinamico": Registros[12],
+              I1: Registros[13],
+              I2: Registros[14],
+              I3: Registros[15],
+            };
+            clientMQTT.publish("tst/Registros", JSON.stringify(arr));
+            //clientMQTT.publish('/tst/Registros', JSON.stringify(Registros))
+          })
+          .catch(function () {
+            console.error(
+              "clientMQTT readCoils " +
+                require("util").inspect(arguments, { depth: null })
+            );
+            clientMQTT.publish("tst/Registros", "OFFLINE");
+          });
+      }
+    });
+  }, 1000);
+});
 
 // Receive messages
-clientMQTT.on('message', function (topic, message) {
-    if (topic.includes('/tst/coilon') || topic.includes('/tst/coiloff')){
-        id = parseInt(message)
-        if (id != isNaN) {
-            let status = socket.resume()._readableState.destroyed
-            status ? socket.connect(options) : "";
+clientMQTT.on("message", function (topic, message) {
+  if (topic.includes("tst/coilon") || topic.includes("/tst/coiloff")) {
+    id = parseInt(message);
+    if (id != isNaN) {
+      let status = socket.resume()._readableState.destroyed;
+      status ? socket.connect(options) : "";
 
-            switch (topic){
-                // Encendemos la bobina
-                case '/tst/coilon': clientPLC.writeSingleCoil(id, true); break;
-                // Apagamos la bobina
-                case '/tst/coiloff': clientPLC.writeSingleCoil(id, false); break;
-            }
-        }
+      switch (topic) {
+        // Encendemos la bobina
+        case "tst/coilon":
+          clientPLC.writeSingleCoil(id, true);
+          break;
+        // Apagamos la bobina
+        case "tst/coiloff":
+          clientPLC.writeSingleCoil(id, false);
+          break;
+      }
     }
-})
-
-
-
+  }
+});
 
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------\
 |   FUNCIONES DE LA API (lECTURA DE REGISTROS   /   LECTURA DE REGISTROS    /   ENCEDER BOBINAS /   APAGAR BOBINAS  /   ALMACENAR INFORMACION EN LA BASE DE DATOS)      |
 \----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 // Fubnción pare lectura de bobinas
-let readCoils = ( req, res ) => {
-    let status = socket.resume()._readableState.destroyed
-    status ? socket.connect(options) : "";
+let readCoils = (req, res) => {
+  let status = socket.resume()._readableState.destroyed;
+  status ? socket.connect(options) : "";
 
-    const { start, end } = req.body
-    clientPLC.readCoils(start, end).then(result => {
-        let Bobinas = result.response._body._valuesAsArray
-        res.status(200).json(Bobinas)
-    }).catch(function () {
-        console.error( "readCoils " + require('util').inspect(arguments, { depth: null}))
-        res.status(202).json(false)
+  const { start, end } = req.body;
+  clientPLC
+    .readCoils(start, end)
+    .then((result) => {
+      let Bobinas = result.response._body._valuesAsArray;
+      res.status(200).json(Bobinas);
     })
-}
+    .catch(function () {
+      console.error(
+        "readCoils " + require("util").inspect(arguments, { depth: null })
+      );
+      res.status(202).json(false);
+    });
+};
 
 // Fubnción pare lectura de bobinas
-let readHoldingRegisters = ( req, res ) => {
-    let status = socket.resume()._readableState.destroyed
-    status ? socket.connect(options) : "";
+let readHoldingRegisters = (req, res) => {
+  let status = socket.resume()._readableState.destroyed;
+  status ? socket.connect(options) : "";
 
-    const { start, end } = req.body
-    clientPLC.readHoldingRegisters(start, end).then(result => {
-        let Registros = result.response._body._valuesAsArray
-        res.status(200).json(Registros)
-    }).catch(function () {
-        console.error( "readHoldingRegisters " + require('util').inspect(arguments, { depth: null}))
-        res.status(202).json(false)
+  const { start, end } = req.body;
+  clientPLC
+    .readHoldingRegisters(start, end)
+    .then((result) => {
+      let Registros = result.response._body._valuesAsArray;
+      res.status(200).json(Registros);
     })
-}
+    .catch(function () {
+      console.error(
+        "readHoldingRegisters " +
+          require("util").inspect(arguments, { depth: null })
+      );
+      res.status(202).json(false);
+    });
+};
 
 // Función para encedido de bobinas
 let coilON = (req, res) => {
-    let status = socket.resume()._readableState.destroyed
-    status ? socket.connect(options) : "";
+  let status = socket.resume()._readableState.destroyed;
+  status ? socket.connect(options) : "";
 
-    const id = req.body.id == undefined ? req.params.id : req.body.id;
+  const id = req.body.id == undefined ? req.params.id : req.body.id;
 
-    clientPLC.writeSingleCoil(id, true).then(result => {
-        res.status(200).json(result.response._body)
-    }).catch(function () {
-        console.error( "coilON " + require('util').inspect(arguments, { depth: null}))
-        res.status(202).json(false)
+  clientPLC
+    .writeSingleCoil(id, true)
+    .then((result) => {
+      res.status(200).json(result.response._body);
     })
-}
+    .catch(function () {
+      console.error(
+        "coilON " + require("util").inspect(arguments, { depth: null })
+      );
+      res.status(202).json(false);
+    });
+};
 
 // Función para apagado de bobinas
 let coilOFF = (req, res) => {
-    let status = socket.resume()._readableState.destroyed
-    status ? socket.connect(options) : "";
-    const id = req.body.id == undefined ? req.params.id : req.body.id;
+  let status = socket.resume()._readableState.destroyed;
+  status ? socket.connect(options) : "";
+  const id = req.body.id == undefined ? req.params.id : req.body.id;
 
-    clientPLC.writeSingleCoil(id, false).then(result => {
-        res.status(200).json(result.response._body)
-    }).catch(function () {
-        console.error( "coilOFF " + require('util').inspect(arguments, { depth: null}))
-        res.status(202).json(false)
+  clientPLC
+    .writeSingleCoil(id, false)
+    .then((result) => {
+      res.status(200).json(result.response._body);
     })
-}
-
+    .catch(function () {
+      console.error(
+        "coilOFF " + require("util").inspect(arguments, { depth: null })
+      );
+      res.status(202).json(false);
+    });
+};
 
 // Función para escritura de bobinas
 let writeSingleRegister = (req, res) => {
-    let status = socket.resume()._readableState.destroyed
-    status ? socket.connect(options) : "";
-    const { id, value } = req.body
+  let status = socket.resume()._readableState.destroyed;
+  status ? socket.connect(options) : "";
+  const { id, value } = req.body;
 
-    clientPLC.writeSingleRegister(id, value).then(result => {
-        res.status(200).json(result.response._body)
-    }).catch(function () {
-        console.error( "writeSingleRegister " + require('util').inspect(arguments, { depth: null}))
-        res.status(202).json(false)
+  clientPLC
+    .writeSingleRegister(id, value)
+    .then((result) => {
+      res.status(200).json(result.response._body);
     })
-}
-
+    .catch(function () {
+      console.error(
+        "writeSingleRegister " +
+          require("util").inspect(arguments, { depth: null })
+      );
+      res.status(202).json(false);
+    });
+};
 
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------\
 |                                                                            CONSULTAS A LA BD                                                                          |
@@ -213,29 +251,32 @@ let data = (req, res) => {
 |                                                                 PETICIONES A LA API POR METODO GET Y POST                                                             |
 \----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
-app.get('/', (req, res) => { res.json({ mensaje: 'Api jsmodbus metodo GET :)' })})
-app.post('/', (req, res) => {res.json({ mensaje: 'Api jsmodbus metodo POST :)' })})
+app.get("/", (req, res) => {
+  res.json({ mensaje: "Api jsmodbus metodo GET :)" });
+});
+app.post("/", (req, res) => {
+  res.json({ mensaje: "Api jsmodbus metodo POST :)" });
+});
 
 // base de datos
-    //app.get('/data', data)
-   // app.post('/data', data)
+//app.get('/data', data)
+// app.post('/data', data)
 
 // LECTURA DE BOBINAS
-    app.post('/readCoils', readCoils)   // Parametros { start, end }
+app.post("/readCoils", readCoils); // Parametros { start, end }
 
 // LECTURA DE REGISTROS
-    app.post('/readRegisters', readHoldingRegisters)    // Parametros { start, end }
+app.post("/readRegisters", readHoldingRegisters); // Parametros { start, end }
 
 // ESCRITURA DE REGISTROS
-    app.post('/writeRegister', writeSingleRegister)    // Parametros { id, value }
+app.post("/writeRegister", writeSingleRegister); // Parametros { id, value }
 
 // ENCEDIDO DE BOBINAS
-    app.get('/coilon/:id', coilON)
-    app.post('/coilon', coilON) // Parametros { id }
+app.get("/coilon/:id", coilON);
+app.post("/coilon", coilON); // Parametros { id }
 
 // APAGADO DE BOBINAS
-    app.get('/coiloff/:id', coilOFF)
-    app.post('/coiloff', coilOFF)    // Parametros { id }
+app.get("/coiloff/:id", coilOFF);
+app.post("/coiloff", coilOFF); // Parametros { id }
 
-
-module.exports = app
+module.exports = app;
